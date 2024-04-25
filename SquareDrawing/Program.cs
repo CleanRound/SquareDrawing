ff71f9809835f5725d6bb5b26e243015cd9aafac
Олег Луцик <oleglutsik.dev@gmail.com>
2024-04-25 11:57:35 +0300

    Добавьте файлы проекта.

---
+++ SquareDrawing/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter a symbol to draw the square: ");
+        char symbol = Convert.ToChar(Console.ReadLine());
+
+        Console.Write("Enter the length of the side of the square: ");
+        int sideLength = Convert.ToInt32(Console.ReadLine());
+
+        DrawSquare(sideLength, symbol);
+    }
+
+    static void DrawSquare(int sideLength, char symbol)
+    {
+        for (int i = 0; i < sideLength; i++)
+        {
+            for (int j = 0; j < sideLength; j++)
+            {
+                if (i == 0 || i == sideLength - 1 || j == 0 || j == sideLength - 1)
+                {
+                    Console.Write(symbol);
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
